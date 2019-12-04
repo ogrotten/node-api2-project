0258f0a5aaaab902f120648cd78b21b184e3768f
Darrin Lowery <lambda@monsto.com>
2019-12-03 18:13:00 -0600

    completed CRUD for posts.

--- api/posts.js
+++ api/posts.js
@@ -8,51 +8,84 @@ function clg(...x) {
 
 postRouter.use(express.json());
 
+
+// basic READ all posts
 postRouter.get("/", (req, res) => {
-	// basic READ all posts
 	db.find(req.query)
-		.then(posts => {
-			clg(">>> GET posts all");
-			res.status(200).json(posts);
-		})
-		.catch(error => {
-			res.status(500).json({
-				msg: "Error READing all posts."
-			});
+	.then(posts => {
+		clg(">>> GET posts all");
+		res.status(200).json(posts);
+	})
+	.catch(error => {
+		res.status(500).json({
+			msg: "Error READing all posts."
 		});
 	});
-	
-	postRouter.get("/:id", (req, res) => {
-		db.findById(req.params.id)
+});
+
+// READ one post
+postRouter.get("/:id", (req, res) => {
+	db.findById(req.params.id)
 		.then(post => {
 			clg(">>> GET one post");
 			post.length > 0
 				? res.status(200).json(post)
-				: res.status(404).json({ msg: "Nonexistant Post." })
+				: res.status(404).json({ msg: "Nonexistant Post. (READ)" })
 		})
 		.catch(err => {
 			clg(err);
 			res.status(500).json({
 				msg: "Error READing one single post."
-			})
-		})
-})
+			});
+		});
+});
 
+// CREATE single new post
 postRouter.post("/", (req, res) => {
-	// CREATE single new post
 	db.insert(req.body)
-		.then(post => {
-			clg(">>> POST new post");
-			res.status(201).json(post);
+	.then(post => {
+		clg(">>> POST new post");
+		res.status(201).json(post);
+	})
+	.catch(err => {
+		clg(err);
+		res.status(500).json({
+			msg: "Error CREATEing new post."
 		})
-		.catch(err => {
-			clg(err);
-			res.status(500).json({
-				msg: "Error CREATEing new post."
-			})
+	})
+});
+
+// UPDATE single existing post
+postRouter.put("/:id", (req, res) => {
+	db.update(req.params.id, req.body)
+	.then(post => {
+		clg(`>>> PUT successful? ${post}`);
+		post
+			? res.status(200).json(req.body)
+			: res.status(404).json({ msg: "Nonexistant Post. (UPDATE)" })
+	})
+	.catch(err => {
+		clg(err);
+		res.status(500).json({
+			msg: "Error UPDATEing existing post."
 		})
+	})
 })
 
-
+postRouter.delete("/:id", (req, res) => {
+	db.remove(req.params.id)
+	.then(post => {
+		clg(`>>> DELETE successful? ${post}`);
+		post
+			? res.status(200).json({ msg: `Deleted Post ${req.params.id}. (DELETE)` })
+			: res.status(404).json({ msg: `Post ${req.params.id} Nonexistant. (DELETE)` })
+	})
+	.catch(err => {
+		clg(err);
+		res.status(500).json({
+			msg: "Error DELETINGing existing post."
+		})
+	})
+})
 
 module.exports = postRouter;
